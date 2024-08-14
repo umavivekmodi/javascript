0da4e323a31c3b355fe71b68d37dd8ad10c01c7a
umavivekmodi <uumad.modi@gmail.com>
2024-08-14 07:20:37 +0000

    dev configration

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 123443
+let accountEmail= "uma@google.com"
+var accountPassword = "0987"
+accountCity = "jaipur"
+
+//  accountId =  2 // not allowed
+// console.log(accountId); 
+
+accountEmail = "um@gm.com"
+accountPassword= "121212"
+accountCity= "goa"
+
+console.table([accountId , accountEmail,accountPassword , accountCity])
