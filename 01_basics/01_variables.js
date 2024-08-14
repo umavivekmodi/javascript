c3e55569b41e52de2232c256ebe4f765408cb25d
umavivekmodi <uumad.modi@gmail.com>
2024-08-14 07:52:44 +0000

    about variables

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,13 +1,17 @@
-const accountId = 123443
+const  accountId = 123443
 let accountEmail= "uma@google.com"
 var accountPassword = "0987"
 accountCity = "jaipur"
+let accountState; 
 
 //  accountId =  2 // not allowed
 // console.log(accountId); 
-
+/*
+perfer not to  use  var
+because of issue in block scope  and functional scope
+*/
 accountEmail = "um@gm.com"
 accountPassword= "121212"
 accountCity= "goa"
 
-console.table([accountId , accountEmail,accountPassword , accountCity])
+console.table([accountId , accountEmail,accountPassword , accountCity  , accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,5 @@
+"use strict"; //  treat all JS code as newr version
+// alert( 3 + 3)//  we are using node Js, not  browser
+
+console.log(3 + 3)
+console.log("hitesh")
