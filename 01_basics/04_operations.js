5f7892787c1ca8eabf00d6af97dad106784fcb19
umavivekmodi <uumad.modi@gmail.com>
2024-08-16 17:12:04 +0000

    operations

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,43 @@
+let value = 3
+let negValue =-value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(5-2);
+// console.log(2*2);
+// console.log(2**3);  // power
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2= " hitesh"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);      //12
+// console.log (1+"2");       // 12
+// console.log ("1"+"2");     // 12
+// console.log("1"+ 2 + 2);   //122
+// console.log(1 + 2 + "2");   //32
+// console.log((3 + 4) * 5 % 3);  TRIKY not used in code
+
+// console.log(true);  //true
+// console.log(+true);  //1
+// console.log(+"");  //0    =>  not  to  use this type of code
+// let num1 , num2 , num3
+// num1 = num2 = num3= 2+2   =>  not  to  use this type of code
+
+let gamecounter = 100
+gamecounter++; //postfix
+++gamecounter;  //prefix
+console.log(gamecounter);
+
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operatons.html#sec-type-convwesion
+
+
+
+
+
+
