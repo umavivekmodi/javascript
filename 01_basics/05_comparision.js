5b17cc638bb71a267ba98dab93142f1a40e94ae6
umavivekmodi <uumad.modi@gmail.com>
2024-08-16 17:33:50 +0000

    05 file of compair

---
+++ 01_basics/05_comparision.js
@@ -0,0 +1,29 @@
+/*console.log(2>1);
+console.log(2>=1);
+console.log(2 <1);
+console.log(2 == 1);
+console.log(2 != 1);*/
+
+
+// console.log("3">1);  //true
+// console.log("02">1);  // true
+
+console.log(null > 0);  //f
+console.log(null == 0);  //f
+console.log(null >= 0);  //t      value conversion 
+console.log(null < 0);   //f
+console.log(null <= 0 );  //t
+
+console.log(undefined == 0);
+console.log(undefined );
+console.log();
+console.log();
+console.log();
+
+
+
+
+
+
+
+
