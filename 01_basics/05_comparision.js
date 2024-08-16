1b5223b1517dbdf851adf8a68d845181750997a5
umavivekmodi <uumad.modi@gmail.com>
2024-08-16 17:42:50 +0000

    comparision

--- 01_basics/05_comparision.js
+++ 01_basics/05_comparision.js
@@ -8,17 +8,27 @@ console.log(2 != 1);*/
 // console.log("3">1);  //true
 // console.log("02">1);  // true
 
-console.log(null > 0);  //f
-console.log(null == 0);  //f
-console.log(null >= 0);  //t      value conversion 
-console.log(null < 0);   //f
-console.log(null <= 0 );  //t
+// console.log(null > 0);  //f
+// console.log(null == 0);  //f
+// console.log(null >= 0);  //t      value conversion 
+// console.log(null < 0);   //f
+// console.log(null <= 0 );  //t
+ 
 
-console.log(undefined == 0);
-console.log(undefined );
-console.log();
-console.log();
-console.log();
+// all are => false answer
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined< 0);
+// console.log(undefined <=0);
+// console.log(undefined >=0);
+
+//strick check =>  ===
+// check value  and check data types also
+
+console.log("2" ===2 );  // false
+
+
+
 
 
 
