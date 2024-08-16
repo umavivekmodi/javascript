b60e569fc5919696a0eb530df20ed5ad69a90496
Uma modi <uumad.modi@gmail.com>
2024-08-16 16:41:09 +0530

    primitive datatype

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,5 +1,59 @@
 "use strict"; //  treat all JS code as newr version
 // alert( 3 + 3)//  we are using node Js, not  browser
 
-console.log(3 + 3)
-console.log("hitesh")
+// console.log(3 + 3)
+// console.log("hitesh")
+
+// ecma script link
+// https://tc39.es/ecma262/#sec-info
+// https://tc39.es/ecma262/
+
+// other one is  => mdn
+
+// dataTypes
+
+let name ="hitesh"                 // string data type
+let age = 28                    // number dataType
+let isLoggedIn = false          //boolean data type
+
+//  primitive  data types 
+
+//  number =>  2 to power 52
+// bigInt =>  is used in trading ,stocks market  and other big website  like facebook ,grenet and all 
+// string  => " "       we use " " double and  single ' ' use but most of useing type is " " double quote
+//Boolean => true / false 
+//null => is a  [standalone value] it is alse a data type => reprsentation an empty value 
+//undefined => we can't defined value but  declear a variable
+//symbol => for uniqueness find 
+
+/*
+
+string , number , bigInt , boolean , symbol, undefined , null 
+
+let name = uma modi 
+let age = 28
+let marrid = true 
+let child = null
+let  job =  undefined 
+let number of  expected salary=  1234569644326269818687;
+const zodic sign =   jemini 
+*/
+
+// non- primitive data  types
+
+//  object
+
+console.log(typeof age);
+
+
+
+
+
+
+
+
+
+
+
+
+
