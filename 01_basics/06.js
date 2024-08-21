849337fe22cc5dcb3f09bb25c34200f9b7b38dc7
umavivekmodi <uumad.modi@gmail.com>
2024-08-21 12:11:41 +0000

    about memory types

---
+++ 01_basics/06.js
@@ -0,0 +1,11 @@
+//Q:- how to work memory allocation:-
+// stack memory(primitive) and Heap memory(Non-primitive)
+//String                              Array
+//Number                              Object
+//null                                Function
+undefined
+Boolean
+Symbol
+BigInt
+// original value kin copy milti h        //original value ka reference milta h 
+// change copy m hota hai                 // change original value mei hota hai
