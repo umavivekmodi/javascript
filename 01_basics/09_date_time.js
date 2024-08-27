44f7f60a0465c077453cfa06d6db4486a1c5c45a
umavivekmodi <uumad.modi@gmail.com>
2024-08-27 13:40:32 +0000

    date function

---
+++ 01_basics/09_date_time.js
@@ -0,0 +1,15 @@
+//Dates
+
+let myDate = new Date();
+console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toISOString());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toJSON());
+console.log(myDate.toLocaleTimeString());
+console.log(myDate.toLocaleString());
+console.log(myDate.toTimeString());
+console.log(myDate.toUTCString());
+
+
