e79a0fc6a9f00fb79fbdad1e9bfc6969b2c059d1
umavivekmodi <uumad.modi@gmail.com>
2024-08-27 13:24:12 +0000

    maths function

--- 01_basics/08_nums_and_maths.js
+++ 01_basics/08_nums_and_maths.js
@@ -27,12 +27,21 @@ const hundreds = 1000000
 
 // ++++++++++++Maths+++++++++++++++++
 
-console.log(Math);
+// console.log(Math);
 
-console.log(Math.abs(-4));
+// console.log(Math.abs(-4));
 
-console.log(Math.round(4.3));
-console.log(Math.round(4.6));
-console.log(Math.round(4.6));
+// console.log(Math.round(4.3));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.5));
+// console.log(Math.min(4,3,7,8,0,2));
+// console.log(Math.max(4,3,7,8,0,2));
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
 
 
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random()*(max - min + 1 ))+ min)
