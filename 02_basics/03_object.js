9282c37f937fb578841334c3b7df393d30a1fea3
umavivekmodi <uumad.modi@gmail.com>
2024-08-30 18:20:26 +0000

    start objects

---
+++ 02_basics/03_object.js
@@ -0,0 +1,17 @@
+// singleton => apne tarh ka ek he object hai , constructure se bnate hai
+// Object.create                      // constructer method isi ke under singleton banta hai 
+
+// litrels ki tarh declear krte hai to singleton nahi bnta hai => object litrelas 
+
+// object litreals = object ko declear krne ka tarika hai ese to dono tarh se object he bante hai 
+ 
+const JsUser = {
+    name :"hitesh",
+    age : 18 ,
+    location : "jaipur",
+    email : "hitesh@google.com",
+    isLoggedIn : false,
+    lastLoginDays : ["Monday" ,"Saturday"]
+}
+
+//access the object =>
