ae90b693a69498c6ff6c2078d8f1c9a579902f4e
umavivekmodi <uumad.modi@gmail.com>
2024-09-02 12:49:09 +0000

    about API AND JSON format

--- 02_basics/05_object.js
+++ 02_basics/05_object.js
@@ -37,10 +37,83 @@ const obj1 = {1: "a" , 2 : "b"}
 const obj2 = {3:"c" , 4:"d"}
 const obj3 = {obj1 , obj2} //object k under object 
 console.log(obj3);
+//sperad operator 
 const obj4 = {...obj1 , ...obj2}  
 console.log(obj4);
+//asign se object ko asign kr skte hai
 const obj5 = Object.assign(obj1 ,obj2)// obj5 or obj 6 are same but { difference}
 console.log(obj5);
 // optional perameter{}
-const obj6 = Object.assign({}, obj1 ,obj2)
+const obj7 = {5:"e" , 6:"f"}
+
+const obj6 = Object.assign({}, obj1 ,obj2 , obj7)
 console.log(obj6);
+// when value comes from data base 
+// array ke under objects 
+
+const users = [
+    {
+        id : 1 , 
+        email : "h@gmail.com"
+    },
+    {
+        id : 2  , 
+        email : "hvd@gmail.com"
+    },
+    {
+        id : 3 , 
+        email : "mn@gmail.com"
+    },
+    {
+        id : 4, 
+        email : "hmu@gmail.com"
+    },
+    {
+        id : 5 , 
+        email : "lk@gmail.com"
+    },
+    {
+        id : 6 , 
+        email : "ty@gmail.com"
+    },
+]
+
+console.log(users[1].email);
+
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));   // tinderuser ka datatype array 
+// all key ko lekr array me rakh diya ab unke uper loops bhi use kr skte hai
+// keys ke jaise he unki vlaue ko bhi nikal skte hai
+console.log(Object.values(tinderUser));  // keys and values dono he array me saperate hokr aa jati hai 
+
+// enteries 
+console.log(Object.entries(tinderUser));  // jo entries kri hai unko saperate array me dikhata hai 
+// means array ke ander array set kr dete hai 
+
+// value ka pta krna ki tinderuser ke pass wo value hai ya nahi
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));  // boolean me result deta hai 
+console.log(tinderUser.hasOwnProperty('isLogged'));  // boolean me result deta hai 
+
+
+// de-structureing 
+// Array ki bhi hoti hai or object ki bhi
+//  object de-structuring 
+
+const coures ={
+    courename :"Js in hindi",
+    prize : "999",
+    courseInstructor: "hiteshChoudhary"
+}
+// coures.courseInstructor
+
+const {courseInstructor :instructor} = coures
+console.log(instructor);
+
+
+// not in js its react 
+// const navbar = ({company}) =>{
+
+// }
+// navbar(company = "hitesh")
+
---
+++ 02_basics/06_object.js
@@ -0,0 +1,24 @@
+// API 
+// means apna kaam kisi or ke sir pr daal dena =>like meanu card of the hotel 
+
+//backend se value JSON type me aati hai
+// look like same object but object has a name but JSON without name in the end wo hold it in a name 
+// ek object jiska koi name nahi hai wo json hota hai
+// open a json => https://api.github.com/users/hiteshchoudhary   // api calls 
+// famous api => randomuserme api  // tool => json formator 
+// {
+//     "name": "hitesh",
+//     "coursename" : "Js in hindi",
+//     "prize" : "free"
+// }
+
+// array ke format ke under apis 
+[
+    {},
+    {},
+    {},
+    {},
+]
+
+
+// javascript object notation=> json 
