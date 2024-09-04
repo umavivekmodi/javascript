3ae03d8a72500068253d8401738670d696706d6f
uma modi <uumad.modi@gmail.com>
2024-09-04 11:29:41 +0000

    IFFE and arrow&this example

--- 03_basics/04_this_&_arrow.js
+++ 03_basics/04_this_&_arrow.js
@@ -1,4 +1,7 @@
 // this means=> current context(value ) 
+// . Global Context
+// In the global context (outside of any function), this refers to the global object. 
+// In a browser, this is typically the window object.
 
 const user = {
     username: "hitesh",
@@ -31,6 +34,19 @@ const user = {
 // }
 // chai()
 
+
+// exe of this function             //child perent ki property ko access kr skta h
+const person = {
+    name: "mahima",
+    greet: function(){
+        console.log(`Hello my name is ${this.name}`)
+        console.log(person);
+    }
+    
+};
+person.greet();
+
+
 //Arrow function      ()=> {}
 const chai = ()=> {
     let userfullname = "hiteshChoudhary"
@@ -62,7 +78,7 @@ const chai = ()=> {
 
 const addtwo= (num1 , num2 )=>( {userFullname: "vivkeumaModi"})  // agr perenthisis ho to return keyword nahi likhna hoga 
    
-   console.log(addtwo(3 ,5));
+//    console.log(addtwo(3 ,5));
 
 //    object  ko return krwane k liye usko parenthisis me rap krna he hoga 
 
@@ -72,3 +88,36 @@ const addtwo= (num1 , num2 )=>( {userFullname: "vivkeumaModi"})  // agr perenthi
 const myArr = [2,5,9,10,74]
 // myArr.forEach(function(){})
 // myArr.forEach(()=>{})
+
+
+    //arrow function example
+
+    // multiply two numbers
+
+    const multiply = (num1 , num2) => num1 * num2 ;
+    // console.log(multiply(4,8));
+
+    const multi = (val1 , val2 )=>(val1 * val2);
+    // console.log(multi(9 ,7))
+
+    // add two string by using arrow function
+
+    const stringAdd = (str1 , str2 )=>(str1 + str2);
+    // console.log(stringAdd("hello", " world."))
+
+    const myName = (string1 ,string2 )=>(string1 + string2);
+    // console.log(myName("uma" ," modi"));
+
+    // greet by add name
+    const greet = name => `Hello ${name}!` ;
+    // console.log(greet("Vivek"));
+
+    const welComeMassage = name=>`Walcome ${name} in our company!`;
+    // console.log(welComeMassage("Uma Modi"));
+
+    // hello massage print 
+    const massage=() => ("Hello World");
+    // console.log(massage());
+
+    const createFunction = (name , age )=> ({name: name ,age: age});
+    // console.log(createFunction('monika' , 28));
