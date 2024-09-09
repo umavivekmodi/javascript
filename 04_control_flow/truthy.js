04195b90ad6aaf0ba8426850e56d521e5c506a06
uma modi <uumad.modi@gmail.com>
2024-09-09 08:35:33 +0000

    Nullish coalescing Operator

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -1,6 +1,6 @@
 // const userEmail = "H@hitesh.ai"              got user email
 // const userEmail = ""                         dont have user email
-// const userEmail = []                            got user email 
+const userEmail = []                           // got user email 
 
 
 if(userEmail){
@@ -15,4 +15,34 @@ else{
 // false , 0 ,-0 , bigInt -  0n, "" , null , undefined , NaN
 // left all falsy value all are truthy value 
 
+// truthy value 
+// true , "0", " ",'false' , [] , {} , Function(){} , 
 
+// for check value 
+
+// if(userEmail.length === 0){
+//     console.log("Array is empty");
+// }
+
+// how to dedect object is empty
+const emptyObj = {}
+if(Object.keys(emptyObj).length === 0){
+console.log("Object is empty");
+}
+
+
+// Nullish coalescing Operator (??): null undefined  => its made for null and undefined 
+
+let val1;
+// val1 = 5  ?? 10
+// val1 =null ?? 10
+// val1 = undefined ?? 15
+// val1 = null ?? undefined
+val1 = null ?? 10 ?? 15
+
+
+console.log(val1);
+
+// terniary Operator
+
+condition ? true : false 
