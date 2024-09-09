39c60841f740bf3f41aef1567e48481715078d4d
uma modi <uumad.modi@gmail.com>
2024-09-09 10:34:24 +0000

    for loop and key words of break and continue

---
+++ 05_loop_iterations/one.js
@@ -0,0 +1,72 @@
+// for loop
+
+for (let i = 0; i <= 10 ; i++) {
+    const element = i;
+    if (element == 5) {
+        // console.log("five is best number");
+    }
+    // console.log(element);
+    
+}
+
+
+
+// for (let index = 0; index <= 10 ; index++) {
+//     const element = index;
+//     console.log(element);
+    
+// }
+
+// for (let i = 0; i < 10; i++) {
+//     console.log(`Outer loop value: ${i}`);
+//     for (let j = 0; j < 10; j++) {
+//     console.log(`inner loop value : ${j} and inner loop :${i}`);
+        
+//     }
+    
+// }
+
+
+
+
+for (let i = 1; i <= 10; i++) {
+    // console.log(`Outer loop value: ${i}`);
+    for (let j = 1; j <= 10; j++) {
+    // console.log(`inner loop value : ${j} and inner loop :${i}`);
+    // console.log(i + `*` + j + '=' + i*j);
+        
+    }
+    
+}
+
+let myArray = ["flash", "batman", "superman"]
+// console.log(myArray.length);
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    // console.log(element);
+}
+
+// key words 
+// break and continue
+
+// for (let i = 1; i <= 20 ; i++) {
+//     if (i ==5) {
+//         console.log(`Detected 5`);
+//         break;        
+//     }
+//     console.log(`value of i is ${i}`);
+    
+// }
+
+
+
+
+//continue
+for (let i = 1; i <= 20 ; i++) {
+    if (i ==5) {
+        console.log(`Detected 5`);
+        continue;        
+    }
+    console.log(`value of i is ${i}`);
+    
+}
