11cf33a05f209281e07ddf33e65e38c4d4c26d11
uma modi <uumad.modi@gmail.com>
2024-09-27 16:58:03 +0000

    property discriptor

--- 10_classes_and_oop/mathpi.js
+++ 10_classes_and_oop/mathpi.js
@@ -11,7 +11,23 @@ console.log(Math.PI);
 const chai = {
     name:"ginger chai",
     prize:250,
-    isAvailable :true
+    isAvailable :true,
+    
+    orderChai: function(){
+        console.log("chai nahi bni");
+    }
 }
 // console.log(chai);
-con
+console.log(Object.getOwnPropertyDescriptor(chai,"name"));
+
+Object.defineProperty(chai , 'name',{
+    writable: false ,
+    enumerable: false
+})
+console.log(Object.getOwnPropertyDescriptor(chai,"name"));
+
+for (let [key , value] of Object.entries(chai)){
+    if(typeof value !== 'function'){
+    console.log(`${key}: ${value}`);
+}
+}
