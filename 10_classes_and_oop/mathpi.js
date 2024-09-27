239340cb5d61c6171abf63a0e70ab88dbc93da52
uma modi <uumad.modi@gmail.com>
2024-09-27 13:13:03 +0000

    object in js

---
+++ 10_classes_and_oop/mathpi.js
@@ -0,0 +1,17 @@
+//hidden chijo k bare m btati hai
+const descripter = Object.getOwnPropertyDescriptor(Math, "PI")
+console.log(descripter);
+
+console.log(Math.PI);
+Math.PI = 5
+console.log(Math.PI);
+
+
+// const mynewObject = Object.create(null)
+const chai = {
+    name:"ginger chai",
+    prize:250,
+    isAvailable :true
+}
+// console.log(chai);
+con
